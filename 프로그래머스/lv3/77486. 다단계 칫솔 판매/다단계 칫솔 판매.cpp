91879049f1c5032cc96239cbb536c6acb2cb9a62
SPIDEY965 <bbnt25@gmail.com>
2023-06-27 15:04:44 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 10.06 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -8,15 +8,15 @@ vector<pair<int,int>> money(10100);
 
 void dadan(int idx, int price)
 {
+    if (price == 0) return;
     if (idx == 0) return;
-    int nm = price < 10 ? 0 : price * 0.1;
-    money[idx].second += (price - nm);
-    dadan(money[idx].first, nm);
+    int temp = price * 0.1;
+    money[idx].second += (price - temp);
+    dadan(money[idx].first, temp);
 }
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
     vector<int> answer;
-    salesman["center"] = 0;
     money[0] = {-1,0};
     
     int cnt = enroll.size();
@@ -32,6 +32,7 @@ vector<int> solution(vector<string> enroll, vector<string> referral, vector<stri
     }
     for (int i = 0; i < seller.size();i++)
         dadan(salesman[seller[i]], amount[i] * 100);
+    
     for (int i = 1; i <= cnt; i++)
         answer.push_back(money[i].second);
     
