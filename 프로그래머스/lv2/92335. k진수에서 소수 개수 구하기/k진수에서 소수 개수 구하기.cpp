6a271367a822da8fccd171c2d978b487c9a82efa
Byeon YuJeong <bbnt25@gmail.com>
2023-09-04 18:47:40 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 string num = "";
-
+// 소수인지 아닌지 판단
 bool checked(long long N)
 {
     if (N <= 1) return 0;
+    // 소수에서는 sqrt(N)보다 작거나 같은 값에서 찾아줌
     for (int i = 2; i <= sqrt(N); i++)
         if (N % i == 0) return 0;
     return 1;
@@ -16,6 +17,7 @@ bool checked(long long N)
 
 int solution(int n, int k) {
     int answer = 0;
+    // k진수로 바꿔주기
     while (n > 0)
     {
         num += to_string(n%k);
@@ -23,16 +25,20 @@ int solution(int n, int k) {
     }
     reverse(num.begin(), num.end());
     
+    // 숫자에서 소수 찾기
     string temp = "";
     for (char alp : num)
     {
+        // 만약 0일때, temp 가 소수인지 아닌지 판단
         if (alp == '0')
         {
             if (temp != "" && checked(stol(temp))) answer++;
             temp = "";
         }
+        // 그외는 temp에 포함시키기
         else temp += alp;
     }
+    // 남은 temp
     if (temp != "" && checked(stol(temp))) answer++;
     return answer;
 }
