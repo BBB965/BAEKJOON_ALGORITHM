e79d42904744a2c8d0783796bc1c1b4f027ac0d0
Byeon YuJeong <bbnt25@gmail.com>
2023-10-05 22:49:41 +0900

    [level 4] Title: 도둑질, Time: 29.37 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.java
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.java
@@ -2,22 +2,20 @@ class Solution {
     public int solution(int[] money) {
         
         int N = money.length;
-        int [] dp0 = new int[N];
-        int [] dp1 = new int[N];
+        int[][] dp = new int[2][N];
         
-        dp0[0] = money[0];
-        dp0[1] = money[1] > money[0] ? money[1] : money[0];
-        
-        dp1[1] = money[1];
+        dp[0][0] = money[0];
+        dp[0][1] = money[1] > money[0] ? money[1] : money[0];
+        dp[1][1] = money[1];
         
         for (int i = 2; i < N - 1; i++)
         {
-            dp0[i] = dp0[i-1] >= dp0[i-2] + money[i] ? dp0[i-1] : dp0[i-2] + money[i];
-            dp1[i] = dp1[i-1] >= dp1[i-2] + money[i] ? dp1[i-1] : dp1[i-2] + money[i];
+            dp[0][i] = dp[0][i-1] >= dp[0][i-2] + money[i] ? dp[0][i-1] : dp[0][i-2] + money[i];
+            dp[1][i] = dp[1][i-1] >= dp[1][i-2] + money[i] ? dp[1][i-1] : dp[1][i-2] + money[i];
         }
         
-        dp1[N-1] = dp1[N-2] >= dp1[N-3] + money[N-1] ? dp1[N-2] : dp1[N-3] + money[N-1];
+        dp[1][N-1] = dp[1][N-2] >= dp[1][N-3] + money[N-1] ? dp[1][N-2] : dp[1][N-3] + money[N-1];
         
-        return Math.max(dp1[N-1], dp0[N-2]);
+        return Math.max(dp[1][N-1], dp[0][N-2]);
     }
 }
