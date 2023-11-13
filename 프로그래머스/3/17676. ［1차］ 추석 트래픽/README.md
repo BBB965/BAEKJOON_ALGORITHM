e97230d926d94073182291d637dcf13cf371675e
Byeon YuJeong <bbnt25@gmail.com>
2023-11-13 14:23:36 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 0.43 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/README.md
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/README.md
@@ -1,10 +1,10 @@
 # [level 3] [1차] 추석 트래픽 - 17676 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17676#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17676) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 0.43 ms
+메모리: 73.5 MB, 시간: 0.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 14:10:18
+2023년 11월 1일 14:23:33
 
 ### 문제 설명
 
--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -4,23 +4,23 @@ class Solution {
     public int solution(String[] lines) {
         int answer = 0;
         
-        ArrayList <int []> arrayList = new ArrayList<>();
+        ArrayList <double []> arrayList = new ArrayList<>();
         
         for (String line : lines) {
             String [] str = line.split(" ");
             String [] time = str[1].split(":");
-            int endSecond = (Integer.parseInt(time[0]) * 3600 + Integer.parseInt(time[1]) * 60) * 1000 + (int) (Double.parseDouble(time[2]) * 1000);
+            double endSecond = (Integer.parseInt(time[0]) * 3600 + Integer.parseInt(time[1]) * 60) + (Double.parseDouble(time[2]));
 
-            int startSecond = endSecond - (int) ((Double.parseDouble(str[2].substring(0, str[2].length() - 1))) * 1000) + 1;
-            int [] temp = {startSecond, endSecond};
+            double startSecond = endSecond - ((Double.parseDouble(str[2].substring(0, str[2].length() - 1)))) + 0.001;
+            double [] temp = {startSecond, endSecond};
             arrayList.add(temp);
         }
         
         for (int i = 0; i < arrayList.size(); i++) {
             int cnt = 1;
-            int endT = arrayList.get(i)[1];
+            double endT = arrayList.get(i)[1];
             for (int j = i + 1; j < arrayList.size(); j++) {
-                if (endT + 1000 > arrayList.get(j)[0]) cnt++;
+                if (endT + 1 > arrayList.get(j)[0]) cnt++;
             }
             if (answer < cnt) answer = cnt;
         }
